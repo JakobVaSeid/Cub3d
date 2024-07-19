294ffae477c593be3b9daf4985feaae9f415f386
Jakob Valentin Seiderer <jakoseid@gmail.com>
2024-07-19 10:58:11 +0200

    fixed color error

--- color_calc.c
+++ color_calc.c
@@ -6,7 +6,7 @@
 /*   By: jseidere <jseidere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 16:04:18 by jseidere          #+#    #+#             */
-/*   Updated: 2024/07/18 14:35:49 by jseidere         ###   ########.fr       */
+/*   Updated: 2024/07/19 10:57:47 by jseidere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,25 @@ void	trible_free(char *s1, char *s2, char *s3)
 
 bool	get_number(t_game *game, char *str)
 {
-	char	*num[3];
 	int		hex;
+	char	**num;
 
-	num[0] = ft_substr(str, 2, len_until_sc(str, ',') - 2);
-	if (!ft_atoi(num[0]))
+	num = ft_split(str + 2, ',');
+	if (!num)
 		return (false);
-	num[1] = ft_substr(str, len_until_sc(str, ',') + 1, \
-	len_until_sc(str + len_until_sc(str, ',') + 1, ','));
-	if (!ft_atoi(num[1]))
-		return (free(num[0]), false);
-	num[2] = ft_substr(str, len_until_sc(str, ',') + \
-	len_until_sc(str + len_until_sc(str, ',') + 1, ',') + 2, ft_strlen(str));
-	if (!ft_atoi(num[2]))
-		return (trible_free(num[0], num[1], num[2]), false);
+	if (!num[0])
+		return (free_map(num), false);
+	if (!num[1])
+		return (free_map(num), false);
+	if (!num[2])
+		return (free_map(num), false);
 	hex = rgb_to_hex(ft_atoi(num[0]), ft_atoi(num[1]), ft_atoi(num[2]));
 	if (str[0] == 'F')
 		game->f_color = hex_to_dec(hex);
 	else if (str[0] == 'C')
 		game->c_color = hex;
 	trible_free(num[0], num[1], num[2]);
+	free(num);
 	return (true);
 }
 
